cfd6a72c00b85fa9677bfff19b88114b1c683161
weerayut0410 <weerayut.la.65@ubu.ac.th>
2023-07-18 12:09:09 +0700

    asd

---
+++ src/contact.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Layout } from "./App";
+
+function Contact() {
+    return(
+    <>
+        <Layout />
+        <h3>Ubon Ratchatani University</h3>
+        <div>
+            85 Sathonlamark Road
+            <br />
+            Warin Chamrap District
+            <br />
+            Ubon Ratchatani
+            <br />
+            Thailand 34190
+        </div>
+    </>
+    );
+}
+
+export default Contact;
